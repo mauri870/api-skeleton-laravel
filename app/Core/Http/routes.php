5f7b1b424de8550d56ca498df5b57b2b9eec85a6
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-11 14:44:50 -0300

    Add login route

--- app/Core/Http/routes.php
+++ app/Core/Http/routes.php
@@ -1,28 +1,15 @@
 <?php
 
-Route::group(['namespace' => 'Auth'], function () {
-//Login
-    Route::get('/login',['as'=>'auth.login','uses'=>'AuthController@getLogin']);
-    Route::post('/login', ['as'=>'auth.post_login','uses'=>'AuthController@postLogin']);
+$api = app('Dingo\Api\Routing\Router');
 
-//Register
-    Route::get('/register', ['as'=>'auth.register','uses'=>'AuthController@getRegister']);
-    Route::post('/register', ['as'=>'auth.post_register','uses'=>'AuthController@postRegister']);
 
-    //Authenticated users can entry
-    Route::group(['middleware' => 'auth'], function () {
 
-        //Logout
-        Route::get('/logout', ['as'=>'auth.logout','uses'=>'AuthController@getLogout']);
+$api->version('v1', function ($api) {
+    $api->post('login', '\App\Core\Http\Controllers\Auth\AuthController@authenticate');
+
+    $api->group(['middleware' => 'api.auth', 'providers' => ['jwt']], function ($api) {
+        // Todo Implement Protected Routes
     });
 
 
-    //Reset password routes
-    // Password reset link request routes...
-    Route::get('password/email', ['as'=>'auth.password.email','uses'=>'PasswordController@getEmail']);
-    Route::post('password/email', ['as'=>'auth.password.postEmail','uses'=>'PasswordController@postEmail']);
-
-    // Password reset routes...
-    Route::get('password/reset/{token}', ['middleware'=>'checkTokenExists','as'=>'auth.reset','uses'=>'PasswordController@getReset']);
-    Route::post('password/reset', ['as'=>'auth.password.postReset','uses'=>'PasswordController@postReset']);
 });
