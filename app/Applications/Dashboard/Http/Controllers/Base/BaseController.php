2c3636797e0e95c1095a39a0d9ed32cda683e91c
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-11 08:04:35 -0300

    Keep Applications directory

--- app/Applications/Dashboard/Http/Controllers/Base/BaseController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Applications\Dashboard\Http\Controllers\Base;
-
-use App\Core\Http\Controllers\Controller;
-
-class BaseController extends Controller
-{
-    protected $view_namespace = 'dashboard::';
-}
--- app/Applications/Dashboard/Http/Requests/Base/BaseRequest.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Applications\Dashboard\Http\Requests\Base;
-
-use App\Core\Http\Requests\Request;
-
-class BaseRequest extends Request
-{
-
-}
