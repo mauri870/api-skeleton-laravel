2fe4d561f789eb0e35c700a5d5c2851174d9ae72
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-11 09:20:20 -0300

    Add APP_VERSION to Application

--- app/Core/Foundation/Application.php
+++ app/Core/Foundation/Application.php
@@ -6,12 +6,19 @@ use Illuminate\Foundation\Application as BaseApplication;
 class Application extends BaseApplication
 {
     /**
+     * The app version.
+     *
+     * @var string
+     */
+    const APP_VERSION = '0.1-dev';
+
+    /**
      * Return the application version
      *
      * @return string
      */
     public function version()
     {
-        return parent::version().PHP_EOL.'<info>Meeting App</info> 0.1-dev';
+        return parent::version().PHP_EOL.'<info>Meeting App</info> '.self::APP_VERSION;
     }
 }
