a41611a316626e6e7c632e42e97ad177d9b09917
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-11 09:14:55 -0300

    Set the application version

--- app/Core/Foundation/Application.php
+++ app/Core/Foundation/Application.php
@@ -5,5 +5,13 @@ use Illuminate\Foundation\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
-
+    /**
+     * Return the application version
+     *
+     * @return string
+     */
+    public function version()
+    {
+        return parent::version().PHP_EOL.'<info>Meeting App</info> 0.1-dev';
+    }
 }
