be8a82d916d22284cb42cd472781e98cbfe83e05
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-11 15:31:24 -0300

    Add route for register users

--- app/Core/Http/routes.php
+++ app/Core/Http/routes.php
@@ -6,10 +6,9 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
     $api->post('login', '\App\Core\Http\Controllers\Auth\AuthController@authenticate');
+    $api->post('register', '\App\Core\Http\Controllers\Auth\AuthController@register');
 
     $api->group(['middleware' => 'api.auth', 'providers' => ['jwt']], function ($api) {
         // Todo Implement Protected Routes
     });
-
-
 });
