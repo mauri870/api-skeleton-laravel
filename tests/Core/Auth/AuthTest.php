10383b3d586f79820703103e5bf777076490729a
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-11 15:21:41 -0300

    Test the register and login routes

--- tests/Core/Auth/AuthTest.php
+++ tests/Core/Auth/AuthTest.php
@@ -2,43 +2,30 @@
 
 namespace App\Tests\Core\Auth;
 
-use App\Tests\AbstractTestCase;
+use App\Tests\ApiTestCase;
 
-class AuthTest extends AbstractTestCase
+class AuthTest extends ApiTestCase
 {
+    public function test_can_register_a_user() {
+        $this->post('register',
+            [
+                'name' => 'New User',
+                'email' => 'new@new.com',
+                'password' => 12345678,
+            ]
+        )->see('token');
 
-    public function test_login_route()
-    {
-        $this->visit('/login')
-            ->see('Faça Login para acessar o Sistema');
+        $this->seeInDatabase('users',['name' => 'New User', 'email' => 'new@new.com']);
+
     }
 
-    public function test_register_route()
-    {
-        $this->visit('/register')
-            ->see('Registre-se');
-    }
+    public function test_can_login_a_user() {
+        $this->post('login',
+            [
+                'email' => 'test@test.com',
+                'password' => 12345678,
+            ]
+        )->see('token');
 
-    public function test_can_register_a_new_user()
-    {
-        $this->visit('/register')
-            ->type('Example', 'name')
-            ->type('example@example.com', 'email')
-            ->type('12345678', 'password')
-            ->type('12345678', 'password_confirmation')
-            ->press('Registrar-se')
-            ->seePageIs('/dashboard')
-            ->assertResponseStatus(200);
-    }
-
-    public function test_can_login_an_existent_user()
-    {
-        $this->visit('/login')
-            ->type('test@test.com', 'email')
-            ->type('12345678', 'password')
-            ->check('remember')
-            ->press('Entrar')
-            ->seePageIs('/dashboard')
-            ->assertResponseStatus(200);
     }
 }
