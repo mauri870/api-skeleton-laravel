9d021d1d78da98d0e5ebcbdf3663d2e2c48dfb2e
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-12 09:50:00 -0300

    Overwrite response method wrapping errors into errors array

--- app/Core/Http/Requests/Request.php
+++ app/Core/Http/Requests/Request.php
@@ -3,8 +3,24 @@
 namespace App\Core\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
 
 abstract class Request extends FormRequest
 {
-    //
+    /**
+     * Get the proper failed validation response for the request.
+     *
+     * @param  array  $errors
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function response(array $errors)
+    {
+        if ($this->ajax() || $this->wantsJson()) {
+            return new JsonResponse(['errors' => $errors], 422);
+        }
+
+        return $this->redirector->to($this->getRedirectUrl())
+            ->withInput($this->except($this->dontFlash))
+            ->withErrors($errors, $this->errorBag);
+    }
 }
