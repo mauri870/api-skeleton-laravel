1c253fc4df2f3dbba4226db78198a7e8334d139c
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-12 11:25:05 -0300

    Set the accept header fot this api

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -2,12 +2,29 @@
 
 namespace App\Tests;
 
+use App\Tests\Traits\InteractsWithApi;
+
 abstract class ApiTestCase extends AbstractTestCase
 {
+    use InteractsWithApi;
+
     /**
      * The base URL to use while testing the application.
      *
      * @var string
      */
     protected $baseUrl = 'http://localhost/api';
+
+    /**
+     * The accept header for this API
+     *
+     * @var string
+     */
+    protected $accept_header;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->accept_header = 'application/'.env('API_STANDARDS_TREE').'.'.env('API_SUBTYPE').'.v1+json';
+    }
 }
