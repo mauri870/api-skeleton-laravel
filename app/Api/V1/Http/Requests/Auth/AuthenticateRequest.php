642f05d22351d55f1fd6743d4323884f40591c27
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-12 09:51:24 -0300

    Add AuthenticateRequest

--- app/Api/V1/Http/Requests/Auth/AuthenticateRequest.php
+++ app/Api/V1/Http/Requests/Auth/AuthenticateRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Api\V1\Http\Requests;
+namespace App\Api\V1\Http\Requests\Auth;
 
 use App\Core\Http\Requests\Request;
 
-class AuthRequest extends Request
+class AuthenticateRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,21 @@ class AuthRequest extends Request
     public function rules()
     {
         return [
-            //
+            'email' => 'required|email',
+            'password' => 'required'
+        ];
+    }
+
+    /**
+     * Set custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name' => 'nome',
+            'password' => 'senha',
         ];
     }
 }
