347dd682b9e042e10718a4a43c1b219820fc245a
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-12 08:54:13 -0300

    Comment unused middlewares

--- app/Core/Http/Kernel.php
+++ app/Core/Http/Kernel.php
@@ -13,10 +13,10 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Core\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class
+        //\App\Core\Http\Middleware\EncryptCookies::class,
+        //\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        //\Illuminate\Session\Middleware\StartSession::class,
+        //\Illuminate\View\Middleware\ShareErrorsFromSession::class
     ];
 
     /**
