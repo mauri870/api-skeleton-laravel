b934c64575ef2262bdca578b45d931f2a5fd8550
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-12 11:25:42 -0300

    Add methods assertIfTokenIsValid and callAPI

--- tests/Traits/InteractsWithApi.php
+++ tests/Traits/InteractsWithApi.php
@@ -1,14 +1,40 @@
 <?php
-/**
- * @author    Mauri de Souza Nunes <mauri870@gmail.com>
- * @copyright Copyright (c) 2016, Mauri de Souza Nunes <github.com/mauri870>
- * @license   https://opensource.org/licenses/MIT MIT License
- */
 
-namespace App\tests\Traits;
-
+namespace App\Tests\Traits;
 
 trait InteractsWithApi
 {
+    public function assertIfTokenIsValid($token)
+    {
+        $response = $this->call(
+            'GET',
+            'validate_token',
+            [],
+            [],
+            [],
+            [
+                'HTTP_Authorization' => 'Bearer '.$token
+            ]
+        );
+        $this->assertResponseOk();
+        $this->seeJsonEquals(json_decode($response->content(), 1), ['status' => 'success']);
+    }
 
+    /**
+     * Call the given API URI and return the Response.
+     *
+     * @param  string  $method
+     * @param  string  $uri
+     * @param  array   $parameters
+     * @param  array   $cookies
+     * @param  array   $files
+     * @param  array   $server
+     * @param  string  $content
+     * @return \Illuminate\Http\Response
+     */
+    public function callAPI($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        $server = array_merge(['HTTP_ACCEPT' => $this->accept_header], $server);
+        return $this->call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
 }
