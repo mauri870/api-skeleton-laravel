1ca5ce893beb853f39c08c84efed71e858c2c266
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-13 12:23:26 -0300

    Add user repository

---
+++ tests/Core/BindingsTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2016, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace App\tests\Core;
+
+
+class BindingsTest
+{
+
+}
