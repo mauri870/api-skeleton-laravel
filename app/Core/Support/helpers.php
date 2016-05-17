383b6ee75daf39b53e9dbbb2958ebb27a2c1a660
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-17 08:08:32 -0300

    Fix wrong cnpj generation

--- app/Core/Support/helpers.php
+++ app/Core/Support/helpers.php
@@ -19,23 +19,20 @@ function random_cnpj($withDots = false)
     $n11 = 0;
     $n12 = 1;
     $d1 = $n12 * 2 + $n11 * 3 + $n10 * 4 + $n9 * 5 + $n8 * 6 + $n7 * 7 + $n6 * 8 + $n5 * 9 + $n4 * 2 + $n3 * 3 + $n2 * 4 + $n1 * 5;
-    $d1 = 11 - (round($d1 - (floor($d1/11)*$d1)));
+    $d1 = 11 - (round($d1 - (floor($d1/11)*11)));
     if ($d1 >= 10) {
         $d1 = 0;
     }
     $d2 = $d1 * 2 + $n12 * 3 + $n11 * 4 + $n10 * 5 + $n9 * 6 + $n8 * 7 + $n7 * 8 + $n6 * 9 + $n5 * 2 + $n4 * 3 + $n3 * 4 + $n2 * 5 + $n1 * 6;
-    $d2 = 11 - (round($d2 - (floor($d2/11)*$d2)));
+    $d2 = 11 - (round($d2 - (floor($d2/11)*11)));
     if ($d2 >= 10) {
         $d2 = 0;
     }
-    $retorno = '';
+    $value = '';
     if ($withDots == 1) {
-        $retorno = '' . $n1 . $n2 . "." . $n3 . $n4 . $n5 . "." . $n6 . $n7 . $n8 . "/" . $n9 . $n10 . $n11 . $n12 . "-" . $d1 . $d2;
+        $value = '' . $n1 . $n2 . "." . $n3 . $n4 . $n5 . "." . $n6 . $n7 . $n8 . "/" . $n9 . $n10 . $n11 . $n12 . "-" . $d1 . $d2;
     } else {
-        $retorno = '' . $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $n10 . $n11 . $n12 . $d1 . $d2;
+        $value = '' . $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $n10 . $n11 . $n12 . $d1 . $d2;
     }
-    return $retorno;
+    return $value;
 }
-
-
-
