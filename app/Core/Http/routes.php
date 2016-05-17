47ea42006b2a0aa5a916f2a7f3a519c876d97e0b
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-17 10:15:02 -0300

    Overwrite old dingo/api routes

--- app/Core/Http/routes.php
+++ app/Core/Http/routes.php
@@ -1,15 +1,11 @@
 <?php
 
-$api = app('Dingo\Api\Routing\Router');
+Route::group(['prefix' => 'api', 'namespace' => 'App\Core\Api\V1\Http\Controllers', 'middleware' => 'cors'], function () {
+    Route::post('login', 'Auth\AuthController@authenticate');
+    Route::post('register', 'Auth\AuthController@register');
 
-$api->version('v1', function ($api) {
-    $api->group(['namespace' => 'App\Core\Api\V1\Http\Controllers', 'middleware' => 'cors'], function ($api) {
-        $api->post('login', 'Auth\AuthController@authenticate');
-        $api->post('register', 'Auth\AuthController@register');
-
-        $api->group(['middleware' => 'jwt.auth'], function ($api) {
-            // Todo Implement Protected Routes
-            $api->get('validate_token', 'Auth\AuthController@validateToken');
-        });
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        // Todo Implement Protected routes
+        Route::get('validate_token', 'Auth\AuthController@validateToken');
     });
 });
