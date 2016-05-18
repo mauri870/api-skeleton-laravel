8ff80cf66c19a070325ea50abcfd8e13e8e1438c
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-18 13:48:40 -0300

    Update fillable fields

--- app/Domains/Users/User.php
+++ app/Domains/Users/User.php
@@ -28,7 +28,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password', 'company_name', 'cnpj', 'address', 'city', 'telephone'];
+    protected $fillable = ['name', 'email', 'password', 'address', 'telephone'];
 
     /**
      * The attributes excluded from the model's JSON form.
