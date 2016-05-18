50594cccbfb94e83e05acf7b5847fd8bd84c0381
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-18 08:11:12 -0300

    Register bindings for plans repository

--- app/Core/Providers/CoreServiceProvider.php
+++ app/Core/Providers/CoreServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Core\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Domains\Users\Repositories\UserRepositoryInterface;
+use App\Domains\Plans\Repositories\PlanRepositoryEloquent;
 use App\Domains\Users\Repositories\UserRepositoryEloquent;
+use App\Domains\Users\Repositories\UserRepositoryInterface;
+use App\Domains\Plans\Repositories\PlanRepositoryInterface;
 
 class CoreServiceProvider extends ServiceProvider
 {
@@ -16,6 +18,7 @@ class CoreServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepositoryEloquent::class);
+        $this->app->bind(PlanRepositoryInterface::class, PlanRepositoryEloquent::class);
     }
 
     /**
