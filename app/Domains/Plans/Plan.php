207e539c515a99fb8c85bba7f2065f92d8ac1381
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-18 08:10:49 -0300

    Add repository and model for plans

---
+++ app/Domains/Plans/Plan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domains\Plans;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    //
+}
---
+++ app/Domains/Plans/Repositories/PlanRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domains\Plans\Repositories;
+
+
+interface PlanRepositoryInterface
+{
+    //
+}
