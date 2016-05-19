3c2c2e970a270992daf274b6763333a6c4011074
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-19 10:07:12 -0300

    Add route for swagger mapping

--- app/Applications/Api/V1/Http/routes.php
+++ app/Applications/Api/V1/Http/routes.php
@@ -5,6 +5,15 @@ Route::group(['prefix' => 'api', 'middleware' => 'cors'], function () {
         Route::post('login', 'Auth\AuthController@authenticate');
         Route::post('register', 'Auth\AuthController@register');
 
+        Route::get('swagger.json', function (){
+            $file = storage_path('app/swagger.json');
+            if (file_exists($file)) {
+                return response()->json(json_decode(file_get_contents($file)));
+            }
+
+            abort(404);
+        });
+
         Route::group(['middleware' => 'jwt.auth'], function () {
             Route::get('me', 'Auth\AuthController@me');
         });
