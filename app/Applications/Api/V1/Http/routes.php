3f79d4308b14ef8ee6bb7d6eda7c6346d952f21f
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-19 10:29:00 -0300

    Add annotations for swagger

--- app/Applications/Api/V1/Http/routes.php
+++ app/Applications/Api/V1/Http/routes.php
@@ -1,9 +1,47 @@
 <?php
 
+/**
+ * @SWG\Swagger(schemes={"http"},
+ *     basePath="/api/v1"
+ * )
+ * @SWG\Info(title="API Skeleton", version="v1")
+ */
+
 Route::group(['prefix' => 'api', 'middleware' => 'cors'], function () {
+
     Route::group(['prefix' => 'v1'], function (){
+
+        /**
+         * @SWG\Post(path="/api/v1/login",
+         *      summary="Login a user",
+         *      produces={"application/json"},
+         *     @SWG\Response(response="200",
+         *      description="Ok"),
+         * )
+         */
+
         Route::post('login', 'Auth\AuthController@authenticate');
+
+        /**
+         * @SWG\Post(path="/api/v1/register",
+         *      summary="Register a new user",
+         *      produces={"application/json"},
+         *     @SWG\Response(response="200",
+         *      description="Ok"),
+         * )
+         */
+
         Route::post('register', 'Auth\AuthController@register');
+
+
+        /**
+         * @SWG\Get(path="/api/v1/swagger.json",
+         *      summary="Return a swagger json map",
+         *      produces={"application/json"},
+         *     @SWG\Response(response="200",
+         *      description="Ok"),
+         * )
+         */
 
         Route::get('swagger.json', function (){
             $file = storage_path('app/swagger.json');
@@ -15,6 +53,16 @@ Route::group(['prefix' => 'api', 'middleware' => 'cors'], function () {
         });
 
         Route::group(['middleware' => 'jwt.auth'], function () {
+
+            /**
+             * @SWG\Get(path="/api/v1/me",
+             *      summary="Return the currently logged in user",
+             *      produces={"application/json"},
+             *     @SWG\Response(response="200",
+             *      description="Ok"),
+             * )
+             */
+
             Route::get('me', 'Auth\AuthController@me');
         });
     });
