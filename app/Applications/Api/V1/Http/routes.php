e8e053b66b9fd1483252a50bb0bbf6e5e7535980
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-23 09:57:53 -0300

    Clean routes file

--- app/Applications/Api/V1/Http/routes.php
+++ app/Applications/Api/V1/Http/routes.php
@@ -10,38 +10,9 @@
 Route::group(['prefix' => 'api', 'middleware' => 'cors'], function () {
 
     Route::group(['prefix' => 'v1'], function (){
-
-        /**
-         * @SWG\Post(path="/api/v1/login",
-         *      summary="Login a user",
-         *      produces={"application/json"},
-         *     @SWG\Response(response="200",
-         *      description="Ok"),
-         * )
-         */
-
         Route::post('login', 'Auth\AuthController@authenticate');
 
-        /**
-         * @SWG\Post(path="/api/v1/register",
-         *      summary="Register a new user",
-         *      produces={"application/json"},
-         *     @SWG\Response(response="200",
-         *      description="Ok"),
-         * )
-         */
-
         Route::post('register', 'Auth\AuthController@register');
-
-
-        /**
-         * @SWG\Get(path="/api/v1/swagger.json",
-         *      summary="Return a swagger json map",
-         *      produces={"application/json"},
-         *     @SWG\Response(response="200",
-         *      description="Ok"),
-         * )
-         */
 
         Route::get('swagger.json', function (){
             $file = storage_path('app/swagger.json');
@@ -53,16 +24,6 @@ Route::group(['prefix' => 'api', 'middleware' => 'cors'], function () {
         });
 
         Route::group(['middleware' => 'jwt.auth'], function () {
-
-            /**
-             * @SWG\Get(path="/api/v1/me",
-             *      summary="Return the currently logged in user",
-             *      produces={"application/json"},
-             *     @SWG\Response(response="200",
-             *      description="Ok"),
-             * )
-             */
-
             Route::get('me', 'Auth\AuthController@me');
         });
     });
