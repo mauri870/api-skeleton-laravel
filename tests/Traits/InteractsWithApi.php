e8d9755ebb76b546495ad752b878d94b4868bc3b
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-06 10:07:13 -0300

    Add method for call api with token

--- tests/Traits/InteractsWithApi.php
+++ tests/Traits/InteractsWithApi.php
@@ -37,4 +37,23 @@ trait InteractsWithApi
         $server = array_merge(['HTTP_ACCEPT' => 'application/json'], $server);
         return $this->call($method, $uri, $parameters, $cookies, $files, $server, $content);
     }
+
+    /**
+     * Call the given API URI with a token and return the Response.
+     *
+     * @param  string  $method
+     * @param  string  $uri
+     * @param  string  $token
+     * @param  array   $parameters
+     * @param  array   $cookies
+     * @param  array   $files
+     * @param  array   $server
+     * @param  string  $content
+     * @return \Illuminate\Http\Response
+     */
+    public function callAPIWithToken($method, $uri, $token, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        $server = array_merge(['HTTP_ACCEPT' => 'application/json', 'HTTP_Authorization' => 'Bearer '.$token], $server);
+        return $this->call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
 }
